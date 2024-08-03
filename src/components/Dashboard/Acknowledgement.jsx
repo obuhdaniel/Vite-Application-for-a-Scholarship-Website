27b1382227bb09747e5a04e1f5730d0090e8188e
obuhdaniel <danielobuh05@gmail.com>
2024-08-03 16:04:22 +0530

    imported jspdf

--- src/components/Dashboard/Acknowledgement.jsx
+++ src/components/Dashboard/Acknowledgement.jsx
@@ -2,7 +2,22 @@ import React from "react";
 import DashboardLayout from "./DashboardLayout";
 import './styles/acknowledgment.css'; 
 
+import jsPDF from "jspdf";
+
 const Acknowledgement = () => {
+    const handleDownload = () => {
+        const doc = new jsPDF();
+    
+        doc.text("Acknowledgment Form", 10, 10);
+        doc.text("Please review and acknowledge the information below:", 10, 20);
+        doc.text("User Name: Obuh Daniel", 10, 30);
+        doc.text("Email: Dano@g.com", 10, 40);
+        doc.text("Date: 12-22-3445", 10, 50);
+        doc.text("Signature: obuh", 10, 60);
+    
+        doc.save('acknowledgment-form.pdf');
+      };
+    
     return(
         <DashboardLayout>
         <div className="form-container">
@@ -18,7 +33,7 @@ const Acknowledgement = () => {
 
            <h4>Click the link below to download your Acknowledgement Card</h4>
           
-            <button type="submit" className="submit-button">
+            <button type="submit" className="submit-button" onClick={handleDownload}>
               Download Acknowledgement Card
             </button>
           </form>
