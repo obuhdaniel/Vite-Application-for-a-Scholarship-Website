199502d69d0563ca2ce8afc298bbbf0f323fa724
obuhdaniel <danielobuh05@gmail.com>
2024-08-05 11:29:00 +0100

    created a template dashboard component to test protected routes

---
+++ src/components/Dashboard/Main.jsx
@@ -0,0 +1,15 @@
+import React, { useContext } from 'react';
+import { AuthContext } from '../Security/AuthContext';
+
+const Dashboard = () => {
+  const { user, logout } = useContext(AuthContext);
+
+  return (
+    <div>
+      <h1>Welcome, {user?.name}</h1>
+      <button onClick={logout}>Logout</button>
+    </div>
+  );
+};
+
+export default Dashboard;
